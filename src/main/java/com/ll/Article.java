6dbe7cbc59207d8519134a8df7146bee51bca543
0n0ng <hy991006@gmail.com>
2025-07-20 22:32:54 +0900

    게시í게시판 수정까지 완료

---
+++ src/main/java/com/ll/Article.java
@@ -0,0 +1,16 @@
+package com.ll;
+
+public class Article {
+    int id;
+    String title;
+    String content;
+    
+    Article(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public void add(Article article) {
+    }
+}
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,33 +1,18 @@
 package com.ll;
 
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("==게시판입니다==");
 
-        while (true) {
+        // 기능별 쪼개서 (App)class만든 뒤 연결시키는 코드.
+        new App(sc).run();
 
-            System.out.print("명령) ");
-            String command = sc.nextLine();
-
-
-            if (command.equals("등록")) {
-                System.out.print("제목 : ");
-                command = sc.nextLine();
-                System.out.print("내용 : ");
-                command = sc.nextLine();
-            }
-
-            System.out.print("종료");
-            command = sc.nextLine();
-
-            break;
-        }
         sc.close();
     }
 }
+
 
 
